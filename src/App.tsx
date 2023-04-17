f0209d0f342f145b5f65775dedcd2f61284290d2
MmYYAIS <39291075+MmYYAIS@users.noreply.github.com>
2023-04-17 14:18:59 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -174,9 +174,6 @@ function App() {
       if (listening && !finalTranscript) {
         return State.LISTENING;
       }
-      if (listening && finalTranscript) {
-        return State.PROCESSING;
-      }
       if (
         (oldState === State.LISTENING && transcript) || // At this point finalTranscript may not have a value yet
         oldState === State.PROCESSING // Avoid setting state to IDLE when transcript is set to '' while processing
@@ -238,6 +235,7 @@ function App() {
         parentMessageId: conversationRef.current.currentMessageId || undefined,
       });
     
+      Voice.stopListening();
     
       if (data) {
         conversationRef.current.currentMessageId = data.messageId;
